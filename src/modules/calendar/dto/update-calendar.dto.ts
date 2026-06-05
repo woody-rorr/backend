a88e6afcf51868d5109bab28a1df7c09f69169b0
pinetech <pinetech@pinetechs-MacBook-Pro.local>
2026-06-05 12:13:13 +0900

    init

--- src/modules/calendar/dto/update-calendar.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCalendarDto } from './create-calendar.dto';
-
-export class UpdateCalendarDto extends PartialType(CreateCalendarDto) {}
--- src/modules/follow/__tests__/follow.e2e-spec.ts
+++
@@ -1,1 +0,0 @@
-// e2e test - see previous scope output
